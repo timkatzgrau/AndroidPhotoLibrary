584346fed0039d49aa31fa68aa92d8c8d439e85f
Katzgrau <Tim@katzgrau.com>
2017-12-07 02:43:14 -0500

    Testing

--- app/build/generated/source/r/debug/android/support/transition/R.java
+++ app/build/generated/source/r/debug/android/support/transition/R.java
@@ -86,51 +86,51 @@ public final class R {
         public static final int italic = 0x7f080051;
         public static final int line1 = 0x7f080055;
         public static final int line3 = 0x7f080056;
-        public static final int media_actions = 0x7f08005b;
-        public static final int normal = 0x7f080066;
-        public static final int notification_background = 0x7f080067;
-        public static final int notification_main_column = 0x7f080068;
-        public static final int notification_main_column_container = 0x7f080069;
-        public static final int parent_matrix = 0x7f08006e;
-        public static final int right_icon = 0x7f080074;
-        public static final int right_side = 0x7f080075;
-        public static final int save_image_matrix = 0x7f080076;
-        public static final int save_non_transition_alpha = 0x7f080077;
-        public static final int save_scale_type = 0x7f080078;
-        public static final int status_bar_latest_event_content = 0x7f08009a;
-        public static final int text = 0x7f08009e;
-        public static final int text2 = 0x7f08009f;
-        public static final int time = 0x7f0800a5;
-        public static final int title = 0x7f0800a6;
-        public static final int transition_current_scene = 0x7f0800ac;
-        public static final int transition_layout_save = 0x7f0800ad;
-        public static final int transition_position = 0x7f0800ae;
-        public static final int transition_scene_layoutid_cache = 0x7f0800af;
-        public static final int transition_transform = 0x7f0800b0;
+        public static final int media_actions = 0x7f08005c;
+        public static final int normal = 0x7f08006a;
+        public static final int notification_background = 0x7f08006b;
+        public static final int notification_main_column = 0x7f08006c;
+        public static final int notification_main_column_container = 0x7f08006d;
+        public static final int parent_matrix = 0x7f080072;
+        public static final int right_icon = 0x7f080078;
+        public static final int right_side = 0x7f080079;
+        public static final int save_image_matrix = 0x7f08007a;
+        public static final int save_non_transition_alpha = 0x7f08007b;
+        public static final int save_scale_type = 0x7f08007c;
+        public static final int status_bar_latest_event_content = 0x7f08009e;
+        public static final int text = 0x7f0800a2;
+        public static final int text2 = 0x7f0800a3;
+        public static final int time = 0x7f0800a9;
+        public static final int title = 0x7f0800aa;
+        public static final int transition_current_scene = 0x7f0800b0;
+        public static final int transition_layout_save = 0x7f0800b1;
+        public static final int transition_position = 0x7f0800b2;
+        public static final int transition_scene_layoutid_cache = 0x7f0800b3;
+        public static final int transition_transform = 0x7f0800b4;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090004;
         public static final int status_bar_notification_info_maxnum = 0x7f090009;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a002a;
-        public static final int notification_action_tombstone = 0x7f0a002b;
-        public static final int notification_media_action = 0x7f0a002c;
-        public static final int notification_media_cancel_action = 0x7f0a002d;
-        public static final int notification_template_big_media = 0x7f0a002e;
-        public static final int notification_template_big_media_custom = 0x7f0a002f;
-        public static final int notification_template_big_media_narrow = 0x7f0a0030;
-        public static final int notification_template_big_media_narrow_custom = 0x7f0a0031;
-        public static final int notification_template_custom_big = 0x7f0a0032;
-        public static final int notification_template_icon_group = 0x7f0a0033;
-        public static final int notification_template_lines_media = 0x7f0a0034;
-        public static final int notification_template_media = 0x7f0a0035;
-        public static final int notification_template_media_custom = 0x7f0a0036;
-        public static final int notification_template_part_chronometer = 0x7f0a0037;
-        public static final int notification_template_part_time = 0x7f0a0038;
+        public static final int notification_action = 0x7f0a002b;
+        public static final int notification_action_tombstone = 0x7f0a002c;
+        public static final int notification_media_action = 0x7f0a002d;
+        public static final int notification_media_cancel_action = 0x7f0a002e;
+        public static final int notification_template_big_media = 0x7f0a002f;
+        public static final int notification_template_big_media_custom = 0x7f0a0030;
+        public static final int notification_template_big_media_narrow = 0x7f0a0031;
+        public static final int notification_template_big_media_narrow_custom = 0x7f0a0032;
+        public static final int notification_template_custom_big = 0x7f0a0033;
+        public static final int notification_template_icon_group = 0x7f0a0034;
+        public static final int notification_template_lines_media = 0x7f0a0035;
+        public static final int notification_template_media = 0x7f0a0036;
+        public static final int notification_template_media_custom = 0x7f0a0037;
+        public static final int notification_template_part_chronometer = 0x7f0a0038;
+        public static final int notification_template_part_time = 0x7f0a0039;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0d0029;
+        public static final int status_bar_notification_info_overflow = 0x7f0d002a;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f0e0100;
---
+++ app/src/main/java/com/example/katzgrau/photosandroid57/openalbum.java
@@ -0,0 +1,122 @@
+package com.example.katzgrau.photosandroid57;
+
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class openalbum extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private Context ctx = this;
+    private String m_Text = "";
+    private ListView listView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    Builder builder = new Builder(ctx);
+                    builder.setTitle("Title");
+
+                    // Set up the input
+                    final EditText input = new EditText(ctx);
+                    // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                    builder.setView(input);
+
+                    // Set up the buttons
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            m_Text = input.getText().toString();
+                            Instagram.getApp().createAlbum(m_Text);
+                            reload();
+                        }
+
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    builder.show();
+                    return true;
+            }
+            return false;
+        }
+    };
+    //Test
+
+    public void reload() {
+        listView.setAdapter(null);
+        ArrayList<String> values = new ArrayList<String>();
+        for(int i = 0; i < Instagram.getApp().getAllAlbums().size(); i++) {
+            values.add(Instagram.getApp().getAllAlbums().get(i).name);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
+        reload();
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+        });
+
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+
+}
