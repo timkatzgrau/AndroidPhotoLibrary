3ddd2aac60757dfeeef046d32f208611eb911417
Tim Katzgrau <Katzgrau@Tims-Air.home>
2017-12-07 01:51:33 -0500

    Displays Album Names

--- app/src/main/java/com/example/katzgrau/photosandroid57/MainActivity.java
+++ app/src/main/java/com/example/katzgrau/photosandroid57/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.katzgrau.photosandroid57;
 
 import android.app.AlertDialog;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,16 +10,25 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.view.View;
+import android.app.Activity;
 import android.app.AlertDialog.Builder;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     private Context ctx = this;
     private String m_Text = "";
-
+    private ListView listView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -44,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
                         public void onClick(DialogInterface dialog, int which) {
                             m_Text = input.getText().toString();
                             Instagram.getApp().createAlbum(m_Text);
+                            reload();
                         }
 
                     });
@@ -61,14 +72,53 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    public void reload() {
+        listView.setAdapter(null);
+        ArrayList<String> values = new ArrayList<String>();
+        for(int i = 0; i < Instagram.getApp().getAllAlbums().size(); i++) {
+            values.add(Instagram.getApp().getAllAlbums().get(i).name);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list);
+        reload();
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+        });
+
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
+
 
 }
